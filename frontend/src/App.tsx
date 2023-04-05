ab49c23c686b6812b936c218f168419d146dd8bf
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2023-04-05 10:43:09 -0600

    Add Navigation Template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Podcast from "./pages/Podcast";
+import Collection from "./pages/Collection";
 
 function App() {
   return (
-   <h1>Hi</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/podcast" element={<Podcast />} />
+        <Route path="/collection" element={<Collection />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Podcast.tsx
@@ -0,0 +1,7 @@
+function Podcast() {
+    return (
+      <h1>Podcast</h1>
+    );
+  }
+  
+  export default Podcast;
